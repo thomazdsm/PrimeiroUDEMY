a733996901f7d22eb4c9ac67057d6c6658b31ef7
Thomaz Machado <thomazdsmachado@gmail.com>
2020-05-22 15:28:07 -0300

    Projeto do Triangulo das Aulas da Udemy

---
+++ Primeiro/Models/Product.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Primeiro.Models
+{
+    class Product
+    {
+        //ATRIBUTES
+        private string _name;
+        public double Price { get; set; }
+        public int Amount { get; private set; }
+
+        //CONSTRUCTOR
+        public Product()
+        {
+        }
+        public Product(string name, double price, int amount)
+        {
+            _name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        //PROPERTIES
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                if(value != null && value.Length > 1)
+                {
+                    _name = value;
+                }    
+            }
+        }
+
+        /*GET & SET METHOD
+        public double GetPrice()
+        {
+            return _price;
+        }
+        public void SetPrice(double price)
+        {
+            _price = price;
+        }
+        public int GetAmount()
+        {
+            return _amount;
+        }
+        */
+
+        //METHODS
+        public double TotalStockValue()
+        {
+            return Amount * Price;
+        }
+
+        public void AddProduct(int quantity)
+        {
+            Amount = Amount + quantity;
+        }
+
+        public void RemoveProduct(int quantity)
+        {
+            Amount = Amount - quantity;
+        }
+
+        public override string ToString()
+        {
+            return _name + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Amount + " unidades. Total: $" + TotalStockValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Primeiro/Program.cs
@@ -0,0 +1,39 @@
+﻿using Primeiro.Models;
+using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre os dados do produto:");
+
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product x = new Product();
+
+            x.Name = name;
+            x.Price = price;
+
+            Console.WriteLine("Dados do produto: " + x);
+
+            Console.WriteLine("Produtos a ADICIONAR no estoque:");
+            int quantity = int.Parse(Console.ReadLine());
+            x.AddProduct(quantity);
+            Console.WriteLine("Dados do produto: " + x);
+
+            Console.WriteLine("Produtos a REMOVER do estoque:");
+            quantity = int.Parse(Console.ReadLine());
+            x.RemoveProduct(quantity);
+            Console.WriteLine("Dados do produto: " + x);
+
+        }
+    }
+}
